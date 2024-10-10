c8d4daeee357b6002a8960049a360b6d86ad60aa
Poojan <poojangoyani@gmail.com>
2024-10-10 21:51:00 +0530

    typo bug fix.

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   res.status(404).send(`
     <html>
       <body>
-        <h1>Endpoint undefined</h1>
+        <h1>Welcome.</h1>
         <p>Please go to <a href="https://github.com/Poojan38380/simple-product-management-api">https://github.com/Poojan38380/simple-product-management-api</a> to learn about all the endpoints.</p>
       </body>
     </html>
